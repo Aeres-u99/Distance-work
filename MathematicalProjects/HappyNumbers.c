6c11410daa6886fbc29ed3cc26855e6b57d5bab4
Aeres-u99 <aeres99@hotmail.com>
2018-02-18 18:49:41 +0530

    Update HappyNumbers.c

--- MathematicalProjects/HappyNumbers.c
+++ MathematicalProjects/HappyNumbers.c
@@ -44,4 +44,21 @@ int main(){
 	}
 		return 0;
 }
+/*
+	A short introduction (again!!)
+-----------------------HAPPY NUMBERS---------------------\n
+A happy number is defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its
+digits in base-ten, and repeat the process until the number either equals 1 (where it will stay), or it loops endlessly in a cycle that does not 
+include 1. Those numbers for which this process ends in 1 are happy numbers, while those that do not end in 1 are unhappy numbers (or sad numbers).
+<from wikipedia>
+[Floyd's Cycle detection algorithm]
+Well its kinda tricky for me to understand but still I will try my best to talk about it here
+It goes like this, in a straight line race a fast bike will never encounter a slow bike , i.e once the race begins the faster bike will
+see slower bike directly at the end of line.
+The same is not the case in case of a circular track, rather the faster bike will complete 'cycle/s' before slower one just completes 
+one revolution.So the thing is in case of a loop in a situation( or a loop in linked list as well) we take two pointers a slow paced one
+and a fast paced one, eventually compare their values if they are equal it means there is a cycle and we can simply use that scenario 
+to skip out of infinite loop :D
+Thats what I have understood and tried to apply
+*/
 	
